0383f71f420a8f2cc36f9558060c31bd42e5850d
sssa <313999717@qq.com>
2024-11-10 14:09:18 +0800

    Update ui.rs

--- crates/egui/src/ui.rs
+++ crates/egui/src/ui.rs
@@ -310,7 +310,7 @@ impl Ui {
             placer,
             enabled,
             sizing_pass,
-            menu_state: self..clone(),
+            menu_state: self.menu_state.clone(),
             stack: Arc::new(ui_stack),
             sense,
             min_rect_already_remembered: false,
